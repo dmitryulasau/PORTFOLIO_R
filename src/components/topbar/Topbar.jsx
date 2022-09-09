776f210981addefc1985146926caefe20732b9ef
DMITRY ULASAU <dmitrushok@gmail.com>
2022-09-08 23:42:16 -0400

    topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,18 +8,26 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SmartphoneIcon from "@mui/icons-material/Smartphone";
 import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ height: "70px" }}>
+        <AppBar
+          position="static"
+          sx={{
+            height: "70px",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
           <Toolbar>
             <Typography
-              variant="h6"
+              variant="h4"
               noWrap
               component="a"
-              href=""
+              href="#intro"
               sx={{
                 mr: 2,
                 display: { xs: "flex", md: "flex" },
@@ -27,21 +35,25 @@ export default function Topbar() {
                 fontWeight: 700,
                 color: "inherit",
                 textDecoration: "none",
+                // [breakpoints.down("xs")]: {
+                //   fontSize: "3rem",
+                // },
               }}
             >
-              Dmitry Ulasau
+              ULADA.
             </Typography>
             <SmartphoneIcon
-              sx={{ display: { xs: "flex", md: "flex" }, mr: 1 }}
+              sx={{ display: { xs: "flex", md: "flex" }, mr: 1, ml: 4 }}
             />
             <Typography
               variant="h6"
               noWrap
               component="a"
-              href=""
+              href="callto:+16462383713"
               sx={{
                 mr: 2,
-                display: { xs: "flex", md: "flex" },
+
+                display: { xs: "none", md: "flex" },
                 flexGrow: 1,
                 fontWeight: 700,
                 color: "inherit",
@@ -50,15 +62,16 @@ export default function Topbar() {
             >
               +1 646 238 3713
             </Typography>
-            <EmailIcon sx={{ display: { xs: "flex", md: "flex" }, mr: 1 }} />
+            <EmailIcon
+              sx={{ display: { xs: "flex", md: "flex" }, mr: 1, ml: 3 }}
+            />
             <Typography
               variant="h6"
               noWrap
               component="a"
-              href=""
+              href="mailto:dmitrushok@gmail.com"
               sx={{
-                mr: 2,
-                display: { xs: "flex", md: "flex" },
+                display: { xs: "none", md: "flex" },
                 flexGrow: 1,
                 fontWeight: 700,
                 color: "inherit",
@@ -67,15 +80,15 @@ export default function Topbar() {
             >
               dmitrushok@gmail.com
             </Typography>
-
+          </Toolbar>
+          <Toolbar sx={{ flexGrow: 1, justifyContent: "flex-end" }}>
             <IconButton
               size="large"
               edge="start"
               color="inherit"
               aria-label="menu"
-              sx={{ mr: 2 }}
             >
-              <MenuIcon />
+              <MenuIcon sx={{ fontSize: "40px" }} />
             </IconButton>
           </Toolbar>
         </AppBar>
